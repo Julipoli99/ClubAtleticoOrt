2278aaf5ecdb1dd5726dd073fc2659ea3de7508e
Julipoli99 <Polimenijulian9@gmail.com>
2022-10-25 16:31:18 -0300

    Creado de clases.

---
+++ Models/Cancha.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClubAtleticoOrt.Models
+{
+    public class Cancha
+    {
+        public int Id { get; set; }
+
+        [EnumDataType(typeof(EnumDeporte))] public EnumDeporte Tipo { get; set; }
+
+        public int Estado { get; set; }
+
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClubAtleticoOrt.Models
+{
+    public class Usuario
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contraseña { get; set; }
+        public DateTime FechaInscripto { get; set; }
+
+        public string Telefono { get; set; }
+    }
+}
