ef60999a249ccac1100088c8ce67c691466b22b2
Julipoli99 <Polimenijulian9@gmail.com>
2022-11-14 19:43:35 -0300

    .

---
+++ Models/EnumDeporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClubAtleticoOrt.Models
+{
+    public enum EnumDeporte
+    {
+        CEMENTO,
+        CESPED_SINTETICO,
+        POLVO_LADRILLO
+    }
+}
