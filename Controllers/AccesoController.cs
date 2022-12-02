c440392fafd40a08d35fcf05122eaec000fadb67
Julipoli99 <Polimenijulian9@gmail.com>
2022-12-02 18:55:25 -0300

    Registro terminado desde el controlador y validacion de datos ingresados.

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -3,11 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClubAtleticoOrt.Context;
+using ClubAtleticoOrt.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+
 
 namespace ClubAtleticoOrt.Controllers
 {
     public class AccesoController : Controller
     {
+
+        private readonly ClubDatabaseContext _context;
+
+        public AccesoController(ClubDatabaseContext context)
+        {
+            _context = context;
+        }
+
         public ActionResult Login()
         {
             return View();
@@ -18,5 +33,59 @@ namespace ClubAtleticoOrt.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registro(Usuario usuario)
+        {
+            var usuarios = await _context.Usuarios.ToListAsync();
+            bool EmailExistente = BuscarDatoUsuario("Email", usuarios);
+            bool DniExistente = BuscarDatoUsuario("Dni", usuarios);
+
+            if (ModelState.IsValid && !EmailExistente && !DniExistente)
+            {
+                usuario.Nombre = Request.Form["Nombre"];
+                usuario.Apellido = Request.Form["Apellido"];
+                usuario.Email = Request.Form["Email"];
+                usuario.Contraseña = Request.Form["Contraseña"];
+                usuario.FechaInscripto = DateTime.Now;
+                usuario.Telefono = Request.Form["Telefono"];
+
+                _context.Usuarios.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Login));
+            }
+            EmailExistente = false;
+            DniExistente = false;
+            return View(usuario);
+        }
+
+        private bool BuscarDatoUsuario(string campo, List<Usuario> lista)
+        {
+
+            int i = 0;
+            bool encontrado = false;
+
+            while (i < lista.Count && !encontrado)
+            {
+                if (lista[i].Email == Request.Form[campo])
+                {
+                    encontrado = true;
+                    ViewData["DatoExistente"] = "El " + campo + " ya se encuentra registrado";
+                }
+                else if(lista[i].Dni == Request.Form[campo])
+                {
+                    encontrado = true;
+                    ViewData["DatoExistente"] = "El " + campo + " ya se encuentra registrado";
+                }
+
+                else
+                {
+                    i++;
+                }
+            }
+            return encontrado;
+        }
+
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,6 +10,8 @@ namespace ClubAtleticoOrt.Models
 {
     public class Usuario
     {
+        internal readonly StringValues campo;
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
