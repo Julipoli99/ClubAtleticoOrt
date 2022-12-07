3b7926d03a95c6da98ce32a6c2932cc10888f147
Leonel Hidalgo <118.hidalgo.leonel.1358@gmail.com>
2022-12-07 12:26:59 -0300

    Se ordenan las validaciones de Models Usuario

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -11,6 +11,11 @@ namespace ClubAtleticoOrt.Models
     {
         private const string CAMPO_REQUERIDO = "Este campo es requerido.";
         private const string ELEGIR_CANCHA = "Selecciona un tipo de cancha.";
+        private const string RANGO_HORA_INICIO = "La hora de inicio no puede ser menor a 0 ni mayor a 23.";
+        private const string RANGO_HORA_FIN = "La hora de fin no puede ser menor a 1 ni mayor a 24.";
+        private const string RANGO_CANCHA = "Solo existen 3 canchas.";
+        private const string RANGO_DNI = "El DNI debe contener 8 dígitos.";
+
 
         [Key]
         public int Id { get; set; }
@@ -19,17 +24,21 @@ namespace ClubAtleticoOrt.Models
         public DateTime Fecha { get; set; }
 
         [Required(ErrorMessage = CAMPO_REQUERIDO)]
+        [Range(00, 24, ErrorMessage = RANGO_HORA_INICIO)]
         public int HoraInicio { get; set; }
 
         [Required(ErrorMessage = CAMPO_REQUERIDO)]
+        [Range(01, 25, ErrorMessage = RANGO_HORA_FIN)]
         public int HoraFin { get; set; }
 
         // [EnumDataType(typeof(TipoCancha))]
         [Required(ErrorMessage = ELEGIR_CANCHA)]
         [ForeignKey("Cancha")]
+        [Range(00, 03, ErrorMessage = RANGO_CANCHA)]
         public int id_cancha { get; set; }
 
         /* [ForeignKey("Usuario")]*/
+        [RegularExpression(@"/d{8}", ErrorMessage = RANGO_DNI)]
         public int Nro_Dni { get; set; }
     }
 }
